5a566c45f76e8aa6e0d10296ad9fec47d64b140d
Alumno <Alumno@DAM1-PC9>
2025-03-05 09:37:22 +0100

    Creacion de la clase AlejandroMG

---
+++ PracticaCV_Alejandro/Clase_AlejandroMG.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCV_Alejandro
+{
+    public class Clase_AlejandroMG
+    {
+    }
+}
